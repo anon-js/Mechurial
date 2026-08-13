9c7889196a5e64bcd778c489b1610effb696f9f2
anon <anon@anonui-MacBookAir.local>
2026-08-13 19:55:49 +0900

    init: mechurial 메뉴 추천 사이트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  basePath: '/mechu',
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
